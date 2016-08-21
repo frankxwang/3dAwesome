8b51e01a2d3bebc2d5a14a097655c109f3a58d22
thegithubgeek <wfrankw9@gmail.com>
2016-08-20 19:54:53 -0700

    Added depth

--- src/Draw.java
+++ src/Draw.java
@@ -5,16 +5,19 @@ import java.util.*;
 
 public class Draw {
 	static ArrayList vecs = new ArrayList<Vec3>();
+	public static int H = 1000;
+	public static int W = 500;
+	
 	private void DoStuff() {
 		JFrame frame = new JFrame();
 		
 		FrameDraw panel = new FrameDraw();
-		panel.setSize(new Dimension(1000, 500));
+		panel.setSize(new Dimension(H, W));
 		
-		frame.setPreferredSize(new Dimension(1000, 500));
+		frame.setPreferredSize(new Dimension(H, W));
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(new GridLayout(2, 2));
+		frame.setLayout(new GridLayout(1,1));
 		frame.add(panel);
 		
 		(panel).paintComponent(panel.getGraphics());
@@ -22,16 +25,17 @@ public class Draw {
 		frame.pack();
 	}
 	Draw() {
-		vecs.add(new Vec3(500, 20, 0));
-		((Vec3)vecs.get(0)).Rotate("z", new Vec3(500, 250, 0), -90);
+		vecs.add(new Vec3(500, 250, 0));
+		((Vec3)vecs.get(0)).Rotate("z", new Vec3(H/2, W/2, 0), -90);
 		System.out.println(((Vec3)vecs.get(0)).x + " " + ((Vec3)vecs.get(0)).y + " " + ((Vec3)vecs.get(0)).z);
 		DoStuff();
 	}
 	class FrameDraw extends JPanel {
+		public static final int MAX_DIST = 100;
 		protected void paintComponent(Graphics g) {
 			for (int i = 0; i < vecs.size(); i++) {
 				Vec3 vec = (Vec3) vecs.get(i);
-				g.fillRect((int) vec.x, (int) vec.y, 5, 5);
+				g.fillRect((int)(vec.x - (vec.x - W/2)*vec.z/MAX_DIST), (int) (vec.y - (vec.y - H/2)*vec.z/MAX_DIST), 5, 5);
 			}
 		}
 	}
