c524ad4a43d3d215ac0374a74423036191f5b861
thegithubgeek <wfrankw9@gmail.com>
2016-08-20 18:27:19 -0700

    CHANGES

--- src/Draw.java
+++ src/Draw.java
@@ -7,14 +7,28 @@ public class Draw {
 	static ArrayList vecs = new ArrayList<Vec3>();
 	private void DoStuff() {
 		JFrame frame = new JFrame();
+		
 		FrameDraw panel = new FrameDraw();
+		panel.setSize(new Dimension(1000, 500));
+		
+		frame.setPreferredSize(new Dimension(1000, 500));
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new GridLayout(2, 2));
+		frame.add(panel);
+		
+		(panel).paintComponent(panel.getGraphics());
+		
+		frame.pack();
 	}
 	Draw() {
-		vecs.add(new Vec3(0, 50, 20));
+		vecs.add(new Vec3(500, 20, 0));
+		((Vec3)vecs.get(0)).Rotate("z", new Vec3(500, 250, 0), -90);
+		System.out.println(((Vec3)vecs.get(0)).x + " " + ((Vec3)vecs.get(0)).y + " " + ((Vec3)vecs.get(0)).z);
 		DoStuff();
 	}
-	class FrameDraw extends JFrame {
-		void paintComponent(Graphics g) {
+	class FrameDraw extends JPanel {
+		protected void paintComponent(Graphics g) {
 			for (int i = 0; i < vecs.size(); i++) {
 				Vec3 vec = (Vec3) vecs.get(i);
 				g.fillRect((int) vec.x, (int) vec.y, 5, 5);
--- src/Vec3.java
+++ src/Vec3.java
@@ -12,14 +12,23 @@ public class Vec3 {
 	
 	public void Rotate(String axis, Vec3 point, float deg){
 		if(axis.equals("y")){
-			z = (float) (point.z*Math.cos(deg) - point.x*Math.sin(deg));
-			x = (float) (point.x*Math.cos(deg) + point.z*Math.sin(deg));
+			this.z = getX(point.z, this.z, point.x, this.x, deg);
+			this.x = getY(point.z, this.z, point.x, this.x, deg);
+			System.out.println("hi1");
 		}else if(axis.equals("x")){
-			y = (float) (point.y*Math.cos(deg) - point.x*Math.sin(deg));
-			x = (float) (point.x*Math.cos(deg) + point.y*Math.sin(deg));
+			this.y = getX(point.y, this.y, point.z, this.z, deg);
+			this.z = getY(point.y, this.y, point.z, this.z, deg);
+			System.out.println("hi2");
 		}else if(axis.equals("z")){
-			x = (float) (point.x*Math.cos(deg) - point.y*Math.sin(deg));
-			y = (float) (point.y*Math.cos(deg) + point.x*Math.sin(deg));
+			this.x = getX(point.x, this.x, point.y, this.y, deg);
+			this.y = getY(point.x, this.x, point.y, this.y, deg);
+			System.out.println("hi3");
 		}
+	}
+	public float getX(float ptX, float thX, float ptY, float thY, float deg){
+		return (float) ((ptX - thX)*Math.cos(deg) - (ptY - thY)*Math.sin(deg) + ptX);
+	}
+	public float getY(float ptX, float thX, float ptY, float thY, float deg){
+		return (float) ((ptY - thY)*Math.cos(deg) + (ptX - thX)*Math.sin(deg) + ptY);
 	}
 }
