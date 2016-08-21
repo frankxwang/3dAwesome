399e1d93fe718e8d3409e66945b5a6acae560a0a
thegithubgeek <wfrankw9@gmail.com>
2016-08-20 17:02:21 -0700

    Added rotate

--- src/Vec3.java
+++ src/Vec3.java
@@ -1,12 +1,25 @@
 
 public class Vec3 {
-	public int x;
-	public int y;
-	public int z;
+	public float x;
+	public float y;
+	public float z;
 	
-	Vec3(int x, int y, int z){
+	Vec3(float x, float y, float z){
 		this.x = x;
 		this.y = y;
 		this.z = z;
+	}
+	
+	public void Rotate(String axis, Vec3 point, float deg){
+		if(axis.equals("y")){
+			z = (float) (point.z*Math.cos(deg) - point.x*Math.sin(deg));
+			x = (float) (point.x*Math.cos(deg) + point.z*Math.sin(deg));
+		}else if(axis.equals("x")){
+			y = (float) (point.y*Math.cos(deg) - point.x*Math.sin(deg));
+			x = (float) (point.x*Math.cos(deg) + point.y*Math.sin(deg));
+		}else if(axis.equals("z")){
+			x = (float) (point.x*Math.cos(deg) - point.y*Math.sin(deg));
+			y = (float) (point.y*Math.cos(deg) + point.x*Math.sin(deg));
+		}
 	}
 }
