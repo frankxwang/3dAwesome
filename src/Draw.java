ce879b80c81bfb79a078c88832f064e6eb0ef9d9
thegithubgeek <wfrankw9@gmail.com>
2016-08-26 22:03:00 -0700

    YAY FILL WORKS ._.<) --> DUELL  <-- (>^_^

--- src/Draw.java
+++ src/Draw.java
@@ -69,8 +69,8 @@ public class Draw {
 			g.clearRect(0, 0, W*2, H*2);
 			int[] a1 = {0,4,5,1};
 			int[] a2 = {2,0,1,3};
-			int[] a3 = {4,6,7,5};
-			int[] a4 = {2,6,7,3};
+			int[] a3 = {6,7,5,4};
+			int[] a4 = {6,7,3,2};
 			int[] a5 = {3,7,5,1};
 			int[] a6 = {2,6,4,0};
 			int[][] arrays = {a1, a2, a3, a4, a5, a6};
@@ -82,11 +82,12 @@ public class Draw {
 //			((Integer)(int)(Vec3.midpoint((Vec3)vecs.get(aa2[3]),
 //					(Vec3)vecs.get(aa2[1])).z)));
 			
-			Arrays.sort(arrays, (aa1, aa2) -> 
-			((Integer)(int)(Vec3.midpoint((Vec3)vecs.get(aa1[0]),
-					(Vec3)vecs.get(aa1[2])).z)).compareTo
-			((Integer)(int)(Vec3.midpoint((Vec3)vecs.get(aa2[0]),
-					(Vec3)vecs.get(aa2[2])).z)));
+//			Arrays.sort(arrays, (aa1, aa2) -> 
+//			((Integer)(int)(Vec3.midpoint((Vec3)vecs.get(aa1[0]),
+//					(Vec3)vecs.get(aa1[2])).z)).compareTo
+//			((Integer)(int)(Vec3.midpoint((Vec3)vecs.get(aa2[0]),
+//					(Vec3)vecs.get(aa2[2])).z)));
+			arrays = sortFaces(arrays);
 			
 			for(int[] i:arrays){
 				System.out.print("{");
@@ -151,19 +152,32 @@ public class Draw {
 		private int getY(float y, float z){
 			return (int)(y - (y - H/2)*z/MAX_DIST);
 		}
-		private void sortFaces(int[][] arrays){
+		private int[][] sortFaces(int[][] arrays){
 
-			int[] VecsMidZ = new int[arrays.length];
+			Integer[] VecsMidZ = new Integer[arrays.length];
 			for(int i=0; i<arrays.length; i++){
-				int[] iArray = arrays[i];
+//				int[] iArray = arrays[i];
 //				VecsMidZ[i] = (int)(Vec3.midpoint((Vec3)(vecs.get(iArray[0])), (Vec3)(vecs.get(iArray[2]))).z*10) + i;
 //				VecsMidZ[i] = ((int)(Vec3.midpoint((Vec3)(vecs.get(iArray[0])), (Vec3)(vecs.get(iArray[0]))).z)) * 10 + i;
-
-				VecsMidZ[i] = ((int) ((Vec3)(vecs.get(iArray[0]))).z);
+				Vec3 vec1 = new Vec3(0,0,0);
+				vec1.x = ((Vec3)vecs.get(arrays[i][0])).x;
+				vec1.y = ((Vec3)vecs.get(arrays[i][0])).y;
+				vec1.z = ((Vec3)vecs.get(arrays[i][0])).z;
+				Vec3 vec2 = new Vec3(0,0,0);//(Vec3) vecs.get(arrays[i][2]);
+				vec2.x = ((Vec3)vecs.get(arrays[i][2])).x;
+				vec2.y = ((Vec3)vecs.get(arrays[i][2])).y;
+				vec2.z = ((Vec3)vecs.get(arrays[i][2])).z;
+				vec1.translate(vec2);
+				vec1.x /= 2;
+				vec1.y /= 2;
+				vec1.z /= 2;
+				
+				VecsMidZ[i]=(Integer)(int) vec1.z;
+//				VecsMidZ[i] = ((int) ((Vec3)(vecs.get(iArray[0]))).z);
 				VecsMidZ[i] *= 10;
 				VecsMidZ[i] += i;
 			}
-			Arrays.sort(VecsMidZ);
+			Arrays.sort(VecsMidZ, Collections.reverseOrder());
 			int[] sorted = new int[arrays.length];
 			int[][] temp = new int[arrays.length][arrays[0].length];
 			for(int i=0; i<arrays.length; i++){
@@ -179,6 +193,8 @@ public class Draw {
 				System.out.println(mod);
 				arrays[i] = temp[mod];
 			}
+			
+			return arrays;
 		}
 	}
 }
